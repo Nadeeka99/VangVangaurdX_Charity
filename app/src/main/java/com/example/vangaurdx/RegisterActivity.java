c63f8361aecb27e4caeee0a83ebe0924048e929c
Nadeeka Fernando <alphanvs@gmail.com>
2022-05-01 23:38:29 +0530

    4rd iteration

--- app/src/main/java/com/example/vangaurdx/RegisterActivity.java
+++ app/src/main/java/com/example/vangaurdx/RegisterActivity.java
@@ -1,14 +1,104 @@
 package com.example.vangaurdx;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class RegisterActivity extends AppCompatActivity {
 
+    private EditText UserEmail,UserPassword,UserConfirmPassword;
+    private Button CreateAccountButton;
+    private ProgressDialog loadingBar;
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        mAuth = FirebaseAuth.getInstance();
+        UserEmail = (EditText) findViewById(R.id.register_email);
+        UserPassword = (EditText) findViewById(R.id.register_password);
+        UserConfirmPassword=(EditText) findViewById(R.id.register_confirm_pass);
+        CreateAccountButton = (Button) findViewById(R.id.register_create_account);
+        loadingBar = new ProgressDialog(this);
+
+        CreateAccountButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                CreatNewAccount();
+            }
+        });
+    }
+
+    private void CreatNewAccount()
+    {
+        String email=UserEmail.getText().toString();
+        String password= UserPassword.getText().toString();
+        String confirmPassword=UserConfirmPassword.getText().toString();
+
+        if (TextUtils.isEmpty(email))
+        {
+            Toast.makeText(this, "Please enter your email!", Toast.LENGTH_SHORT).show();
+        }
+        if (TextUtils.isEmpty(password))
+        {
+            Toast.makeText(this, "Please enter your password!", Toast.LENGTH_SHORT).show();
+        }
+        else if (TextUtils.isEmpty(confirmPassword))
+        {
+            Toast.makeText(this, "Please  re enter password!", Toast.LENGTH_SHORT).show();
+        }
+        else if (!password.equals(confirmPassword))
+        {
+            Toast.makeText(this, "Confirmed password does not match!", Toast.LENGTH_SHORT).show();
+        }
+        else
+        {
+            loadingBar.setTitle("Creating New Account");
+            loadingBar.setMessage("Please wait,Account creation in progress.");
+            loadingBar.show();
+            loadingBar.setCanceledOnTouchOutside(true);
+            mAuth.createUserWithEmailAndPassword(email,password)
+                    .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task)
+                        {
+                            if(task.isSuccessful())
+                            {
+                                SendUserToSetupActivity();
+                                Toast.makeText(RegisterActivity.this, "Successfully Authenticated", Toast.LENGTH_SHORT).show();
+
+                            }
+                            else
+                            {
+                                String message = task.getException().getMessage();
+                                Toast.makeText(RegisterActivity.this, "Error Occurred"+message, Toast.LENGTH_SHORT).show();
+                            }
+                            loadingBar.dismiss();
+                        }
+                    });
+        }
+    }
+
+    private void SendUserToSetupActivity()
+    {
+        Intent setupIntent=new Intent(RegisterActivity.this,SetupActivity.class);
+        setupIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(setupIntent);
+        finish();
     }
 }
--- app/src/main/java/com/example/vangaurdx/SetupActivity.java
+++ app/src/main/java/com/example/vangaurdx/SetupActivity.java
@@ -3,12 +3,31 @@ package com.example.vangaurdx;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class SetupActivity extends AppCompatActivity {
 
+    private EditText UserName, FullName, CountryName;
+    private Button SaveInfoButton;
+    private CircleImageView UserProfileImage;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setup);
+
+        UserName= (EditText) findViewById(R.id.set_username);
+        FullName= (EditText) findViewById(R.id.set_fullname);
+        CountryName= (EditText) findViewById(R.id.set_country);
+
+        SaveInfoButton= (Button) findViewById(R.id.set_data_button);
+        UserProfileImage= (CircleImageView) findViewById(R.id.set_user_image);
+
+
+
+
     }
 }
